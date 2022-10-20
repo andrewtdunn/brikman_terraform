0060f9e8aeaaa31f56a39e6f253b824c21d5c019
Andrew Dunn <andrew@triumphtech.com>
2022-10-20 12:17:20 -0500

    initial commit

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0d593311db5abb72b"
+  instance_type = "t2.micro"
+	tags = {
+		"Name" = "terraform-example"
+	}
+}
